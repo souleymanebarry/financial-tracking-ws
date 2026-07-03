9062f790914202f209919f9455c0ccbddeee878d
BARRY <barry.souleymane88@yahoo.com>
2026-07-03 15:49:54 +0200

    refactor(financial-api) ♻️: flatten account DTOs into AccountDTO and consolidate mappers with strict unmapped policy

--- financial-api/src/main/java/com/barry/bank/api/mappers/BankAccountDtoFactory.java
+++
@@ -1,9 +0,0 @@
-package com.barry.bank.api.mappers;
-
-public final class BankAccountDtoFactory {
-
-    public static final String CURRENT_ACCOUNT = "CURRENT ACCOUNT";
-    public static final String SAVING_ACCOUNT  = "SAVING ACCOUNT";
-
-    private BankAccountDtoFactory() {}
-}
